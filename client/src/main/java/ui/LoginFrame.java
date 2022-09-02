bdfdb3ee4fd1ab0fb6bc4810fd4828f852ce21d4
Anna Aplavina <annaaplavina@gmail.com>
2022-09-02 13:50:41 +0300

    finished login window design

--- client/src/main/java/ui/LoginFrame.java
+++ client/src/main/java/ui/LoginFrame.java
@@ -1,24 +1,54 @@
 package ui;
 
+import ui.components.PlaceholderTextField;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.Objects;
 
 public class LoginFrame extends JFrame {
-    JPanel northPanel = new JPanel();
-    JPanel westPanel = new JPanel();
-    JPanel eastPanel = new JPanel();
-    JPanel southPanel = new JPanel();
-    JPanel centerPanel = new JPanel();
-    JLabel mainLabel = new JLabel("LOGIN", SwingConstants.CENTER);
+    private JPanel northPanel;
+    private JPanel westPanel;
+    private JPanel eastPanel;
+    private JPanel southPanel;
+    private JPanel centerPanel;
+    private JLabel mainLabel;
+    private JLabel emojiLabel;
+    private JLabel lockLabel;
+    private PlaceholderTextField usernameField;
+    private PlaceholderTextField passwordField;
+    private JButton signInButton;
+    private JButton registerButton;
+
 
     public LoginFrame(){
+        northPanel = new JPanel();
+        westPanel = new JPanel();
+        eastPanel = new JPanel();
+        southPanel = new JPanel();
+        centerPanel = new JPanel();
+        mainLabel = new JLabel("LOGIN", SwingConstants.CENTER);
+        emojiLabel = new JLabel(new ImageIcon(getClass().getResource("emoji.png")), JLabel.RIGHT);
+        lockLabel = new JLabel(new ImageIcon(getClass().getResource("lock.png")), JLabel.RIGHT);
+        usernameField = new PlaceholderTextField();
+        usernameField.setPlaceholder("Username");
+        passwordField = new PlaceholderTextField();
+        passwordField.setPlaceholder("Password");
+        signInButton = new JButton("Sign in");
+        signInButton.setPreferredSize(new Dimension(165 ,30));
+        registerButton = new JButton("Register");
+        registerButton.setPreferredSize(new Dimension(165, 30));
+
+        Color mainColor = new Color(88, 119, 235);
+
         this.setLayout(new BorderLayout());
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int width = (int) screenSize.getWidth();
         int height = (int) screenSize.getHeight();
         this.setBounds(width/2 - 200, height/2 - 250, 400, 500);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        northPanel.setBackground(new Color(88, 119, 235));
+        northPanel.setBackground(mainColor);
         northPanel.setPreferredSize(new Dimension(400, 110));
         northPanel.setLayout(new GridBagLayout());
         mainLabel.setFont(new Font("OPPO Sans", Font.ITALIC, 35));
@@ -26,20 +56,41 @@ public class LoginFrame extends JFrame {
         northPanel.add(mainLabel);
         this.add(northPanel, BorderLayout.NORTH);
 
-        southPanel.setBackground(new Color(88, 119, 235));
+        southPanel.setBackground(mainColor);
         southPanel.setPreferredSize(new Dimension(400, 75));
         this.add(southPanel, BorderLayout.SOUTH);
 
-        westPanel.setBackground(new Color(88, 119, 235));
+        westPanel.setBackground(mainColor);
         westPanel.setPreferredSize(new Dimension(88, 350));
+        westPanel.setLayout(new GridLayout(9, 1));
+        westPanel.add(emojiLabel);
+        westPanel.add(new JLabel());
+        westPanel.add(lockLabel);
+
         this.add(westPanel, BorderLayout.WEST);
 
-        eastPanel.setBackground(new Color(88, 119, 235));
+        eastPanel.setBackground(mainColor);
         eastPanel.setPreferredSize(new Dimension(88, 350));
         this.add(eastPanel, BorderLayout.EAST);
 
-        centerPanel.setBackground(new Color(88, 119, 235));
+        centerPanel.setBackground(mainColor);
         centerPanel.setPreferredSize(new Dimension(50, 50));
+        centerPanel.setLayout(new GridLayout(9,1));
+        centerPanel.add(usernameField);
+        centerPanel.add(new JLabel());
+        centerPanel.add(passwordField);
+        centerPanel.add(new JLabel());
+        centerPanel.add(new JLabel());
+        JPanel signInHolder = new JPanel();
+        signInHolder.setBackground(mainColor);
+        signInHolder.add(signInButton);
+        centerPanel.add(signInHolder);
+        centerPanel.add(new JLabel());
+        centerPanel.add(new JLabel());
+        JPanel registerHolder = new JPanel();
+        registerHolder.setBackground(mainColor);
+        registerHolder.add(registerButton);
+        centerPanel.add(registerHolder);
         this.add(centerPanel);
     }
 }

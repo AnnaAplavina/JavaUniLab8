e58a51bb86402feff8f75ed6d5237957d9ef2246
Anna Aplavina <annaaplavina@gmail.com>
2022-09-03 13:56:29 +0300

    registering on server

--- client/src/main/java/ui/frames/RegisterFrame.java
+++ client/src/main/java/ui/frames/RegisterFrame.java
@@ -1,13 +1,27 @@
 package ui.frames;
 
+import connection.MusicBandConnection;
+import connection.MusicBandResponse;
+import connection.ResponseStatus;
+import ui.components.PlaceholderPasswordField;
 import ui.components.PlaceholderTextField;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
 public class RegisterFrame extends JFrame {
 
-    public RegisterFrame(){
+    private PlaceholderTextField usernameField = new PlaceholderTextField();
+    private PlaceholderPasswordField passwordField1 = new PlaceholderPasswordField();
+    private PlaceholderPasswordField passwordField2 = new PlaceholderPasswordField();
+    private JLabel usernameCommentLabel = new JLabel();
+    private JLabel passwordCommentLabel = new JLabel();
+    private MusicBandConnection connection;
+
+    public RegisterFrame(MusicBandConnection connection){
+        this.connection = connection;
+
         //design
         JPanel northPanel = new JPanel();
         JPanel westPanel = new JPanel();
@@ -18,12 +32,9 @@ public class RegisterFrame extends JFrame {
         JLabel emojiLabel = new JLabel(new ImageIcon(getClass().getResource("emoji.png")), JLabel.RIGHT);
         JLabel lockLabel1 = new JLabel(new ImageIcon(getClass().getResource("lock.png")), JLabel.RIGHT);
         JLabel lockLabel2 = new JLabel(new ImageIcon(getClass().getResource("lock.png")), JLabel.RIGHT);
-        PlaceholderTextField usernameField = new PlaceholderTextField();
         usernameField.setPlaceholder("Username");
-        PlaceholderTextField passwordField1 = new PlaceholderTextField();
         passwordField1.setPlaceholder("Password");
-        PlaceholderTextField passwordField2 = new PlaceholderTextField();
-        passwordField2.setPlaceholder("Password");
+        passwordField2.setPlaceholder("Repeat password");
         JButton registerButton = new JButton("Register");
         registerButton.setPreferredSize(new Dimension(165, 30));
 
@@ -68,17 +79,58 @@ public class RegisterFrame extends JFrame {
         centerPanel.setPreferredSize(new Dimension(50, 50));
         centerPanel.setLayout(new GridLayout(9,1));
         centerPanel.add(usernameField);
-        centerPanel.add(new JLabel());
+        usernameCommentLabel.setForeground(Color.RED);
+        centerPanel.add(usernameCommentLabel);
         centerPanel.add(new JLabel());
         centerPanel.add(passwordField1);
         centerPanel.add(new JLabel());
         centerPanel.add(passwordField2);
-        centerPanel.add(new JLabel());
+        passwordCommentLabel.setForeground(Color.RED);
+        centerPanel.add(passwordCommentLabel);
         centerPanel.add(new JLabel());
         JPanel registerHolder = new JPanel();
         registerHolder.setBackground(mainColor);
         registerHolder.add(registerButton);
         centerPanel.add(registerHolder);
         this.add(centerPanel);
+
+        //functionality
+        registerButton.addActionListener((e)-> registerOnServer());
+    }
+
+    private void registerOnServer(){
+        String username = usernameField.getText().trim();
+        if(username.equals("")){
+            usernameCommentLabel.setText("Username field can not be empty");
+        }
+        else {
+            String password1 = new String(passwordField1.getPassword());
+            String password2 = new String(passwordField2.getPassword());
+            if(password1.equals("")){
+                passwordCommentLabel.setText("Password field can not be empty");
+            }
+            else if(!password1.equals(password2)){
+                passwordCommentLabel.setText("Passwords are not the same");
+            }
+            else{
+                connection.setUsername(username);
+                connection.setPassword(password1);
+                try {
+                    MusicBandResponse response = connection.sendCommand("register");
+                    if(response.status == ResponseStatus.SUCCESS){
+                        System.out.println("Registration successful");
+                    }
+                    else{
+                        passwordCommentLabel.setText("This username is already used");
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }

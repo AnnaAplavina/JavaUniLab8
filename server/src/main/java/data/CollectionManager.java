f3480fd5f552c9e8432ee1a002d2e6b84c95c507
AnnaAplavina <59871896+AnnaAplavina@users.noreply.github.com>
2023-06-18 16:57:49 +0300

    fixed bugs

--- server/src/main/java/data/CollectionManager.java
+++ server/src/main/java/data/CollectionManager.java
@@ -176,6 +176,7 @@ public class CollectionManager {
         readWriteLock.writeLock().lock();
         try{
             musicBandDao.clearUserBands(username);
+            collection.clear();
             collection.addAll(musicBandDao.getBandsFromDb());
         } finally {
             readWriteLock.writeLock().unlock();
